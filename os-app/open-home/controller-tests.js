99b5b57b49503c4fb2157c85b2176711d5be9b20
Rosano Coutinho <pub@xeari.com>
2018-04-28 12:30:41 -0400

    hello world

---
+++ os-app/open-home/controller-tests.js
@@ -0,0 +1,24 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var assert = require('assert');
+
+var homeController = require('./controller');
+
+describe('OLSKControllerRoutes', function testOLSKControllerRoutes () {
+	
+	it('returns route objects', function () {
+		assert.deepEqual(homeController.OLSKControllerRoutes(), {
+			WKCRouteHome: {
+				OLSKRoutePath: '/',
+				OLSKRouteMethod: 'get',
+				OLSKRouteFunction: homeController.index,
+				OLSKRouteLanguages: ['en'],
+			},
+		});
+	});
+
+});
---
+++ os-app/open-home/controller.js
@@ -0,0 +1,23 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+//_ OLSKControllerRoutes
+
+exports.OLSKControllerRoutes = function () {
+	return {
+		WKCRouteHome: {
+			OLSKRoutePath: '/',
+			OLSKRouteMethod: 'get',
+			OLSKRouteFunction: exports.index,
+			OLSKRouteLanguages: ['en'],
+		},
+	};
+};
+
+exports.index = function (req, res, next) {	
+	res.render(res.locals.OLSKSharedPageControllerSlug + '/index', {
+	});
+};
