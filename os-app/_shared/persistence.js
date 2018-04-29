0c0e486cdf973abb29b8e53f53ee72d82d107288
Rosano Coutinho <pub@xeari.com>
2018-04-28 20:50:39 -0400

    WKCPersistenceNotesAdd

--- os-app/_shared/persistence.js
+++ os-app/_shared/persistence.js
@@ -17,15 +17,43 @@ exports.WKCPersistenceMembers = function (callback) {
 			throw new Error('WKCErrorDatabaseConnectionFailed');
 		}
 
-	  return client.db(kWKCDatabaseName).collection('wkc_members').find({}).toArray(function(err, items) {
-	  	if (err) {
-	  		console.log(err);
-	  		throw new Error('WKCErrorDatabaseFindFailed');
-	  	}
+		return client.db(kWKCDatabaseName).collection('wkc_members').find({}).toArray(function(err, items) {
+			if (err) {
+				console.log(err);
+				throw new Error('WKCErrorDatabaseFindFailed');
+			};
 
-	    client.close();
+			client.close();
 
-	    return callback(items)
-	  })
+			return callback(items);
+		});
+	});
+};
+
+//_ WKCPersistenceNotesAdd
+
+exports.WKCPersistenceNotesAdd = function (callback) {
+	MongoClient.connect(kWKCDatabaseURL, function(err, client) {
+		if (err) {
+			console.log(err);
+			throw new Error('WKCErrorDatabaseConnectionFailed');
+		}
+
+		var noteDate = new Date();
+
+		return client.db(kWKCDatabaseName).collection('wkc_notes').insertOne({
+			WKCNoteBody: noteDate.toLocaleDateString(),
+			WKCNoteDateCreated: noteDate,
+			WKCNoteDateUpdated: noteDate,
+		}, function(err, result) {
+			if (err) {
+				console.log(err);
+				throw new Error('WKCErrorDatabaseFindFailed');
+			};
+
+			client.close();
+
+			return callback(result.ops.pop());
+		});
 	});
 };
