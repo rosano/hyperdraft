3a8144664ee1bfb30ce32cbc435b5bea85fcb944
Rosano Coutinho <pub@xeari.com>
2018-05-17 12:13:07 -0400

    WKCActionAPINotesCreate

--- os-app/auth-api-notes/controller-tests.js
+++ os-app/auth-api-notes/controller-tests.js
@@ -6,21 +6,107 @@
 
 var assert = require('assert');
 
-var apiNotesController = require('./controller');
+describe('Connection', function testConnection() {
 
-describe('OLSKControllerRoutes', function testOLSKControllerRoutes() {
+	var mongodbPackage = require('mongodb');
+	var mongoClient;
 
-	it('returns route objects', function() {
-		assert.deepEqual(apiNotesController.OLSKControllerRoutes(), {
-			WKCRouteAPINotesAdd: {
-				OLSKRoutePath: '/api/notes',
-				OLSKRouteMethod: 'post',
-				OLSKRouteFunction: apiNotesController.WKCActionAPINotesCreate,
-				OLSKRouteMiddlewares: [
-					'WKCSharedMiddlewareAPIAuthenticate',
-				],
-			},
+	before(function(done) {
+		mongodbPackage.MongoClient.connect(process.env.WKC_DATABASE_URL, function(err, client) {
+			if (err) {
+				throw err;
+			}
+
+			mongoClient = client;
+
+			done();
 		});
+	});
+
+	after(function() {
+		if (!mongoClient) {
+			return;
+		}
+
+		mongoClient.close()
+	});
+
+	beforeEach(function() {
+		mongoClient.db(process.env.WKC_SHARED_DATABASE_NAME).dropDatabase();
+	});
+
+	var apiNotesController = require('./controller');
+
+	describe('OLSKControllerRoutes', function testOLSKControllerRoutes() {
+
+		it('returns route objects', function() {
+			assert.deepEqual(apiNotesController.OLSKControllerRoutes(), {
+				WKCRouteAPINotesAdd: {
+					OLSKRoutePath: '/api/notes',
+					OLSKRouteMethod: 'post',
+					OLSKRouteFunction: apiNotesController.WKCActionAPINotesCreate,
+					OLSKRouteMiddlewares: [
+						'WKCSharedMiddlewareAPIAuthenticate',
+					],
+				},
+			});
+		});
+
+	});
+
+	describe('WKCActionAPINotesCreate', function testWKCActionAPINotesCreate() {
+
+		var fakeRequest = function(inputData = {}) {
+			return {
+				OLSKSharedConnectionFor: function() {
+					return {
+						OLSKConnectionClient: mongoClient,
+					}
+				},
+				body: Object.assign({}, inputData),
+				headers: {
+					'x-client-key': process.env.WKC_INSECURE_API_ACCESS_TOKEN,
+				},
+			};
+		};
+
+		var fakeResponse = function() {
+			return {
+				json: function(inputData) {
+					return inputData;
+				},
+			};
+		};
+
+		var fakeResponseAsync = function(callback) {
+			return {
+				json: function(inputData) {
+					return callback(inputData);
+				},
+			};
+		};
+
+		it('returns WKCErrors if no input', function() {
+			assert.deepEqual(apiNotesController.WKCActionAPINotesCreate(fakeRequest(), fakeResponse()), {
+				WKCErrors: {
+					WKCNoteBody: [
+						'WKCErrorNotString',
+					],
+				},
+			});
+		});
+
+		it('returns created object if valid noteObject', function(done) {
+			apiNotesController.WKCActionAPINotesCreate(fakeRequest({
+				WKCNoteBody: 'alpha',
+			}), fakeResponseAsync(function(responseJSON) {
+				assert.deepEqual(responseJSON.WKCNoteBody, 'alpha');
+				assert.deepEqual(responseJSON.WKCNoteDateCreated instanceof Date, true);
+				assert.deepEqual(responseJSON.WKCNoteDateUpdated instanceof Date, true);
+				done();
+			}));
+		});
+
 	});
 
 });
--- os-app/auth-api-notes/controller.js
+++ os-app/auth-api-notes/controller.js
@@ -4,6 +4,8 @@
  * MIT Licensed
  */
 
+var modelLibrary = require('./model');
+
 //_ OLSKControllerRoutes
 
 exports.OLSKControllerRoutes = function() {
@@ -17,4 +19,26 @@ exports.OLSKControllerRoutes = function() {
 			],
 		},
 	};
+};
+
+//_ WKCActionAPINotesCreate
+
+exports.WKCActionAPINotesCreate = function(req, res, next) {
+	var inputData = req.body;
+
+	if (!modelLibrary.WKCModelInputDataIsNotesObject(inputData)) {
+		return res.json(inputData);
+	}
+
+	var noteDate = new Date();
+	return req.OLSKSharedConnectionFor('WKCSharedConnectionMongo').OLSKConnectionClient.db(process.env.WKC_SHARED_DATABASE_NAME).collection('wkc_notes').insertOne(Object.assign(inputData, {
+		WKCNoteDateCreated: noteDate,
+		WKCNoteDateUpdated: noteDate,
+	}), function(err, result) {
+		if (err) {
+			throw new Error('WKCErrorDatabaseCreate');
+		}
+
+		return res.json(result.ops.pop());
+	});
 };
