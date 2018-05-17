baaa285da9ccdc85bf32c445d0cc1bc7a5b799e1
Rosano Coutinho <pub@xeari.com>
2018-05-17 09:17:05 -0400

    WKCModelInputDataIsNotesObject

---
+++ os-app/auth-api-notes/model-tests.js
@@ -0,0 +1,35 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var assert = require('assert');
+
+var notesLibrary = require('./model');
+
+describe('WKCModelInputDataIsNotesObject', function testWKCModelInputDataIsNotesObject() {
+
+	it('returns false if not object', function() {
+		assert.deepEqual(notesLibrary.WKCModelInputDataIsNotesObject(null), false);
+	});
+
+	it('returns false with WKCErrors if WKCNoteBody not string', function() {
+		var item = {
+			WKCNoteBody: null,
+		};
+		assert.deepEqual(notesLibrary.WKCModelInputDataIsNotesObject(item), false);
+		assert.deepEqual(item.WKCErrors, {
+			WKCNoteBody: [
+				'WKCErrorNotString',
+			],
+		});
+	});
+
+	it('returns true if valid noteObject', function() {
+		assert.deepEqual(notesLibrary.WKCModelInputDataIsNotesObject({
+			WKCNoteBody: 'alpha',
+		}), true);
+	});
+
+});
---
+++ os-app/auth-api-notes/model.js
@@ -0,0 +1,28 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+//_ WKCModelInputDataIsNotesObject
+
+exports.WKCModelInputDataIsNotesObject = function(inputData) {
+	if (typeof inputData !== 'object' || inputData === null) {
+		return false;
+	};
+
+	var errors = {};
+
+	if (typeof inputData.WKCNoteBody !== 'string') {
+		errors.WKCNoteBody = [
+			'WKCErrorNotString',
+		];
+	};
+
+	if (Object.keys(errors).length) {
+		inputData.WKCErrors = errors;
+		return false;
+	}
+
+	return true;
+};
