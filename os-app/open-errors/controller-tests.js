244467be96f4f1b42bd9ae9c793b3447422f29ac
Rosano Coutinho <pub@xeari.com>
2018-05-20 17:48:12 -0400

    WKCErrors

---
+++ os-app/open-errors/controller-tests.js
@@ -0,0 +1,82 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var assert = require('assert');
+
+var errorController = require('./controller');
+
+describe('OLSKControllerSharedErrorHandlers', function testOLSKControllerSharedErrorHandlers() {
+
+	it('returns shared error handlers', function() {
+		assert.deepEqual(errorController.OLSKControllerSharedErrorHandlers(), [
+			errorController.WKCErrorsFirstHandler,
+			errorController.WKCErrors404Handler,
+			errorController.WKCErrorsFinalHandler,
+		]);
+	});
+
+});
+
+describe('WKCErrorsFirstHandler', function testWKCErrorsFirstHandler() {
+
+	var pathPackage = require('path');
+
+	it('sets res.locals.OLSKSharedPageControllerSlug to directory name', function() {
+		var res = {
+			locals: {},
+		};
+		errorController.WKCErrorsFirstHandler(new Error('alpha'), null, res, function() {});
+		assert.strictEqual(res.locals.OLSKSharedPageControllerSlug, pathPackage.basename(__dirname));
+	});
+
+	it('returns next(error)', function() {
+		var errorObject = new Error('alpha');
+		assert.deepEqual(errorController.WKCErrorsFirstHandler(errorObject, null, {
+			locals: {},
+		}, function(inputData) {
+			return inputData;
+		}), errorObject);
+	});
+
+});
+
+describe('WKCErrors404Handler', function testWKCErrors404Handler() {
+
+	it('renders page if res.statusCode 404', function() {
+		errorController.WKCErrors404Handler(null, {}, {
+			statusCode: 404,
+			locals: {
+				OLSKSharedPageControllerSlug: 'alpha',
+			},
+			render: function(viewsPath) {
+				assert.strictEqual(viewsPath, 'alpha/404');
+			},
+		}, null);
+	});
+
+	it('returns next(error)', function() {
+		var errorObject = new Error('alpha');
+		assert.deepEqual(errorController.WKCErrors404Handler(errorObject, null, {}, function(inputData) {
+			return inputData;
+		}), errorObject);
+	});
+
+});
+
+describe('WKCErrorsFinalHandler', function testWKCErrorsFinalHandler() {
+
+	it('renders page', function() {
+		errorController.WKCErrorsFinalHandler(null, {}, {
+			locals: {
+				OLSKSharedPageControllerSlug: 'alpha',
+			},
+			render: function(viewsPath) {
+				assert.strictEqual(viewsPath, 'alpha/500');
+			},
+		}, null);
+	});
+
+});
---
+++ os-app/open-errors/controller.js
@@ -0,0 +1,51 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var pathPackage = require('path');
+
+//_ OLSKControllerSharedErrorHandlers
+
+exports.OLSKControllerSharedErrorHandlers = function() {
+	return [
+		exports.WKCErrorsFirstHandler,
+		exports.WKCErrors404Handler,
+		exports.WKCErrorsFinalHandler,
+	];
+};
+
+//_ WKCErrorsFirstHandler
+
+exports.WKCErrorsFirstHandler = function(err, req, res, next) {
+	res.locals.OLSKSharedPageControllerSlug = pathPackage.basename(__dirname);
+
+	return next(err);
+};
+
+//_ WKCErrors404Handler
+
+exports.WKCErrors404Handler = function(err, req, res, next) {
+	if (res.statusCode !== 404) {
+		return next(err);
+	}
+
+	return res.render([
+		res.locals.OLSKSharedPageControllerSlug,
+		'404',
+	].join('/'), {});
+};
+
+//_ WKCErrorsFinalHandler
+
+exports.WKCErrorsFinalHandler = function(err, req, res, next) {
+	if (res.statusCode === 200) {
+		res.status(500);
+	}
+
+	return res.render([
+		res.locals.OLSKSharedPageControllerSlug,
+		'500',
+	].join('/'), {});
+};
