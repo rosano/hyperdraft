b3763453dc1a11318ecf62e7b7dfae0e100b238e
Rosano Coutinho <pub@xeari.com>
2018-05-21 10:13:09 -0400

    Setup frontend assets with gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulpPackage = require('gulp');
+var pathPackage = require('path');
+var filesystemLibrary = require('OLSKFilesystem');
+
+function clean(callback) {
+	filesystemLibrary.OLSKFilesystemHelpDeleteDirectoryRecursive(pathPackage.join(__dirname, 'os-public/shared-assets/vendor'));
+	return callback();
+}
+
+function install() {
+	return gulpPackage.src([
+		'normalize.css',
+		'd3',
+	].map(function(e) {
+		return [
+			[
+				'node_modules',
+				e,
+				'**/*.js',
+			].join('/'), [
+				'node_modules',
+				e,
+				'**/*.css',
+			].join('/')
+		];
+	}).reduce(function(collection, e) {
+		return collection.concat(e);
+	}), []).pipe(gulpPackage.dest(function(vinylFile) {
+		return pathPackage.join('os-public/shared-assets/vendor/', vinylFile.path.replace(pathPackage.join(__dirname, 'node_modules/'), '').split('/').shift());
+	}));
+}
+
+var build = gulpPackage.series(clean, install);
+
+gulpPackage.task('default', build);
