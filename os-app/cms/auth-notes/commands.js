0892e2677d6e2ae409bc79abb28239149ac3cc26
Rosano Coutinho <pub@xeari.com>
2018-09-05 21:27:53 -0400

    Extract WKCommandsDeleteWithConfirmation

--- os-app/cms/auth-notes/commands.js
+++ os-app/cms/auth-notes/commands.js
@@ -61,6 +61,64 @@
 		d3.select('#WKCAppNotesDeleteButton').attr('disabled', null);
 	};
 
+	//_ WKCommandsDeleteWithConfirmation
+
+	exports.WKCommandsDeleteWithConfirmation = function (sharedData) {
+		var persistenceIsCued = !!sharedData.WKCAppNotesSharedPersistenceTask._OLSKTaskTimerID;
+
+		clearInterval(sharedData.WKCAppNotesSharedPersistenceTask._OLSKTaskTimerID);
+
+		if (!window.confirm('<%= OLSKLocalized('WKCAppNotesDeleteConfirmation') %>')) {
+			if (persistenceIsCued) {
+				OLSKTasks.OLSKTasksTimeoutForTaskObject(sharedData.WKCAppNotesSharedPersistenceTask);
+			}
+
+			return;
+		};
+
+		exports._WKCommandsDeleteWithoutConfirmation(sharedData);
+	};
+
+	//_ _WKCommandsDeleteWithoutConfirmation
+
+	exports._WKCommandsDeleteWithoutConfirmation = function (sharedData) {
+		d3.select('#WKCAppNotesPersistenceStatus').text('<%= OLSKLocalized('WKCAppNotesPersistenceStatusDeleting') %>');
+
+		d3.json((<%- OLSKCanonicalSubstitutionFunctionFor('WKCRouteAPINotesDelete') %>)({
+			wkc_note_id: sharedData.WKCAppNotesSharedSelectedItem.WKCNoteID,
+		}), {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-client-key': sharedData.WKCAppNotesSharedAPIToken,
+			},
+		}).then(function(responseJSON) {
+			WKControl.WKReactNoteObjects(d3.selectAll('.WKCAppNotesListItem').data().filter(function(e) {
+				return e !== sharedData.WKCAppNotesSharedSelectedItem;
+			}), sharedData);
+
+			WKControl.WKCommandsSelectNote(d3.selectAll('.WKCAppNotesListItem').data()[0], sharedData);
+
+			d3.select('#WKCAppNotesPersistenceStatus').text('<%= OLSKLocalized('WKCAppNotesPersistenceStatusDeleted') %>');
+
+			setTimeout(function() {
+				d3.select('#WKCAppNotesPersistenceStatus').text('');
+			}, 1000);
+		}, function(error) {
+			d3.select('#WKCAppNotesPersistenceStatus').text('');
+			
+			exports.WKCommandsAlertUnableToDelete(error);
+		});
+	};
+
+	//_ WKCommandsAlertUnableToDelete
+
+	exports.WKCommandsAlertUnableToDelete = function (error) {
+		window.alert('<%= OLSKLocalized('WKCAppNotesPersistenceStatusUnableToDelete') %>');
+
+		throw error;
+	};
+
 	//# REACT
 
 	//_ WKReactNoteObjects
