79055277cf8f3d28ce07534bebcc9a04464ed3d3
Rosano Coutinho <pub@xeari.com>
2018-09-05 21:04:04 -0400

    Extract WKReactNoteObjects

--- os-app/cms/auth-notes/commands.js
+++ os-app/cms/auth-notes/commands.js
@@ -24,6 +24,33 @@
 		throw new Error('WKCAppErrorNotesUnavailable');
 	};
 
+	//_ WKReactNoteObjects
+
+	exports.WKReactNoteObjects = function (noteObjects, sharedData) {
+		var selection = d3.select('#WKCAppNotesList')
+			.selectAll('.WKCAppNotesListItem').data(noteObjects);
+		
+		selection.enter()
+			.append('div')
+				.attr('class', 'WKCAppNotesListItem')
+				.on('click', function(d) {
+					d3.selectAll('.WKCAppNotesListItem').classed('WKCAppNotesListItemSelected', false);
+					d3.select(this).classed('WKCAppNotesListItemSelected', true);
+
+					return exports.WKCNotesAppCommandsSelectItem(d, sharedData);
+				})
+				.merge(selection)
+					.html(function(d) {
+						return [
+							'<pre>',
+							(d.WKCNoteBody || '').split('\n').slice(0, 3).join('\n'),
+							'</pre>',
+						].join('');
+					});
+
+		selection.exit().remove();
+	};
+
 	Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
--- os-public/cms/auth-notes/ui-interaction.js
+++ os-public/cms/auth-notes/ui-interaction.js
@@ -10,33 +10,6 @@
 	(factory((global.WKCNotesAppInteraction = global.WKCNotesAppInteraction || {})));
 }(this, (function (exports) { 'use strict';
 
-	//_ WKCNotesAppPerformDataJoinWithItemsArrayAndSharedData
-
-	exports.WKCNotesAppPerformDataJoinWithItemsArrayAndSharedData = function (itemsArray, sharedData) {
-		var selection = d3.select('#WKCAppNotesList')
-			.selectAll('.WKCAppNotesListItem').data(itemsArray);
-		
-		selection.enter()
-			.append('div')
-				.attr('class', 'WKCAppNotesListItem')
-				.on('click', function(d) {
-					d3.selectAll('.WKCAppNotesListItem').classed('WKCAppNotesListItemSelected', false);
-					d3.select(this).classed('WKCAppNotesListItemSelected', true);
-
-					return exports.WKCNotesAppCommandsSelectItem(d, sharedData);
-				})
-				.merge(selection)
-					.html(function(d) {
-						return [
-							'<pre>',
-							(d.WKCNoteBody || '').split('\n').slice(0, 3).join('\n'),
-							'</pre>',
-						].join('');
-					});
-
-		selection.exit().remove();
-	};
-
 	//_ WKCNotesAppCommandsSelectItem
 
 	exports.WKCNotesAppCommandsSelectItem = function (item, sharedData) {
