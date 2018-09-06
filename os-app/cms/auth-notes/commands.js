5c8f139e9650f95b33d411de5e5c4024539d223f
Rosano Coutinho <pub@xeari.com>
2018-09-05 20:56:45 -0400

    Move token unavailable alert to command

---
+++ os-app/cms/auth-notes/commands.js
@@ -0,0 +1,22 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.WKControl = global.WKControl || {})));
+}(this, (function (exports) { 'use strict';
+
+	//_ WKCNotesAppFoo
+
+	exports.WKCommandsAlertTokenUnavailable = function () {
+		window.alert('<%= OLSKLocalized('WKCNotesErrors').WKCAppErrorTokenUnavailable %>');
+		throw new Error('WKCAppErrorTokenUnavailable');
+	};
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
