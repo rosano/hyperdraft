bd7f16ca7e73298b229c1bd905c446a8457872a2
Rosano Coutinho <pub@xeari.com>
2018-12-07 15:51:09 -0500

    Structure subscriptions behaviour

---
+++ os-app/cms/auth-subscriptions/behaviour.js
@@ -0,0 +1,95 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.WKSubscriptions = global.WKSubscriptions || {})));
+}(this, (function (exports) { 'use strict';
+
+	var moi = exports;
+
+	var WKSubscriptionsPropertyAPIToken;
+
+	//# PROPERTIES
+
+	//_ propertiesAPIToken
+
+	moi.propertiesAPIToken = function (inputData) {
+		if (typeof inputData === 'undefined') {
+			return WKSubscriptionsPropertyAPIToken;
+		}
+
+		WKSubscriptionsPropertyAPIToken = inputData;
+	};
+
+	//# INTERFACE
+
+	//_ interfaceFetchButtonDidClick
+
+	moi.interfaceFetchButtonDidClick = function () {
+		moi.commandsFetchURL();
+	};
+
+	//# COMMANDS
+
+	//_ commandsAlertConnectionError
+
+	moi.commandsAlertConnectionError = function (error) {
+		window.alert('<%= OLSKLocalized('WKCNotesErrors').WKCAppErrorServiceUnavailable %>');
+
+		throw error;
+	};
+
+	//_ commandsAlertTokenUnavailable
+
+	moi.commandsAlertTokenUnavailable = function () {
+		window.alert('<%= OLSKLocalized('WKCNotesErrors').WKCAppErrorTokenUnavailable %>');
+
+		throw new Error('WKCAppErrorTokenUnavailable');
+	};
+
+	//_ commandsFetchURL
+
+	moi.commandsFetchURL = function () {
+		alert('commandsFetchURL');
+	};
+
+	//# SETUP
+
+	//_ setupEverything
+
+	moi.setupEverything = function () {
+		moi.setupAPIToken(function () {});
+	};
+
+	//_ setupAPIToken
+
+	moi.setupAPIToken = function (completionHandler) {
+		d3.json('<%= OLSKCanonicalFor('WKCRouteAPIToken') %>', {
+			method: 'GET',
+		}).then(function(responseJSON) {
+			if (!responseJSON.WKCAPIToken) {
+				return moi.commandsAlertTokenUnavailable();
+			}
+
+			moi.propertiesAPIToken(responseJSON.WKCAPIToken);
+
+			completionHandler();
+		}, moi.commandsAlertConnectionError);
+	};
+
+	//# LIFECYCLE
+
+	//_ lifecyclePageWillLoad
+
+	moi.lifecyclePageWillLoad = function () {
+		moi.setupEverything();
+	};
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
