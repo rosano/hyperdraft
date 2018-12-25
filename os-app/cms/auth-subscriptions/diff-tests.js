bf3f8605eb116eb26953d798c266da07cdd494e9
Rosano Coutinho <pub@xeari.com>
2018-12-25 11:24:57 -0500

    Cleanup tests

--- os-app/cms/auth-subscriptions/diff-tests.js
+++ os-app/cms/auth-subscriptions/diff-tests.js
@@ -9,9 +9,6 @@ var assert = require('assert');
 var diffLibrary = require('./diff');
 
 const kTests = {
-	kTestsRSSInvalid: function() {
-		return '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channell></channel></rss>';
-	},
 	kTestsRSSValid: function() {
 		return '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel><item><title>bravo</title><guid>bravo</guid><description></description></item><item><title>alfa</title><guid>alfa</guid><description></description></item></channel></rss>';
 	},
@@ -42,7 +39,7 @@ const kTests = {
 describe('WKCDiffArticlesForFeed', function testWKCDiffArticlesForFeed() {
 
 	it('returns none if new invalid', function() {
-		assert.deepEqual(diffLibrary.WKCDiffArticlesForFeed(kTests.kTestsRSSValid(), kTests.kTestsRSSInvalid()), []);
+		assert.deepEqual(diffLibrary.WKCDiffArticlesForFeed(kTests.kTestsRSSValid(), kTests.kTestsRSSValid().replace('rss', 'rssx')), []);
 	});
 
 	it('returns all if old empty', function() {
