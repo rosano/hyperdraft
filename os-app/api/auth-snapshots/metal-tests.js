bbadd2e0b5715e08e82993f3384f908145536bf9
Rosano Coutinho <pub@xeari.com>
2018-12-27 09:11:15 -0500

    Spec Snapshot metal

---
+++ os-app/api/auth-snapshots/metal-tests.js
@@ -0,0 +1,63 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var assert = require('assert');
+
+var metalLibrary = require('./metal');
+
+const kTesting = {
+	kTestingValidSnapshot: function() {
+		return {
+			WKCSnapshotBody: 'alfa',
+		};
+	},
+};
+
+describe('WKCSnapshotsMetalCreate', function tesSnapshotsMetalCreate() {
+
+	it('throws error if param2 not object', function() {
+		assert.throws(function() {
+			metalLibrary.WKCSnapshotsMetalCreate(WKCTestingMongoClient, '', function() {});
+		}, /WKCErrorInvalidInput/);
+	});
+
+	it('throws error if param3 not function', function() {
+		assert.throws(function() {
+			metalLibrary.WKCSnapshotsMetalCreate(WKCTestingMongoClient, {}, null);
+		}, /WKCErrorInvalidInput/);
+	});
+
+	it('returns WKCErrors if not valid WKCSnapshot', function(done) {
+		metalLibrary.WKCSnapshotsMetalCreate(WKCTestingMongoClient, {
+			WKCSnapshotBody: null,
+		}, function(err, responseJSON) {
+			assert.deepEqual(responseJSON.WKCErrors, {
+				WKCSnapshotBody: [
+					'WKCErrorNotString',
+				],
+			});
+
+			done();
+		});
+	});
+
+	it('returns WKCSnapshot', function(done) {
+		metalLibrary.WKCSnapshotsMetalCreate(WKCTestingMongoClient, kTesting.kTestingValidSnapshot(), function(err, responseJSON) {
+			assert.deepEqual(responseJSON, Object.assign(kTesting.kTestingValidSnapshot(), {
+				WKCSnapshotID: responseJSON.WKCSnapshotID,
+				WKCSnapshotDateCreated: responseJSON.WKCSnapshotDateCreated,
+				WKCSnapshotDateUpdated: responseJSON.WKCSnapshotDateUpdated,
+			}));
+
+			assert.strictEqual(parseInt(responseJSON.WKCSnapshotID) - (new Date()) > -500, true);
+			assert.strictEqual(responseJSON.WKCSnapshotDateCreated - (new Date()) > -500, true);
+			assert.strictEqual(responseJSON.WKCSnapshotDateUpdated - (new Date()) > -500, true);
+
+			done();
+		});
+	});
+
+});
---
+++ os-app/api/auth-snapshots/metal.js
@@ -0,0 +1,46 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var modelLibrary = require('./model');
+
+//_ WKCSnapshotsMetalCreate
+
+exports.WKCSnapshotsMetalCreate = function(databaseClient, inputData, completionHandler) {
+	if (typeof inputData !== 'object' || inputData === null) {
+		throw new Error('WKCErrorInvalidInput');
+	}
+
+	if (typeof completionHandler !== 'function') {
+		throw new Error('WKCErrorInvalidInput');
+	}
+
+	const errors = modelLibrary.WKCSnapshotsModelErrorsFor(inputData);
+	if (errors) {
+		return completionHandler(null, Object.assign(inputData, {
+			WKCErrors: errors,
+		}));
+	}
+
+	var currentDate = new Date();
+
+	return databaseClient.db(process.env.WKC_SHARED_DATABASE_NAME).collection('wkc_snapshots').insertOne(Object.assign(inputData, {
+		WKCSnapshotID: parseInt(new Date() * 1).toString(),
+		WKCSnapshotDateCreated: currentDate,
+		WKCSnapshotDateUpdated: currentDate,
+	}), function(err, result) {
+		if (err) {
+			return completionHandler(err);
+		}
+
+		var snapshotObject = result.ops.pop();
+
+		modelLibrary.WKCSnapshotHiddenPropertyNames().forEach(function(obj) {
+			delete snapshotObject[obj];
+		});
+
+		return completionHandler(null, snapshotObject);
+	});
+};
