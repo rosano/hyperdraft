effbe10f1446704cc16e44951c3c476c401f88fc
Rosano Coutinho <pub@xeari.com>
2018-12-31 20:14:54 -0500

    Rename WKBehaviour

--- os-public/cms/auth-write/ui-behaviour.js
+++ os-public/cms/auth-write/ui-behaviour.js
@@ -7,7 +7,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.WKBehaviour = global.WKBehaviour || {})));
+	(factory((global.WKCWriteBehaviour = global.WKCWriteBehaviour || {})));
 }(this, (function (exports) { 'use strict';
 
 	var moi = {};
