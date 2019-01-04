b7af013b58e3427502fde20f6737e7f529b033cd
Rosano Coutinho <pub@xeari.com>
2019-01-04 12:25:23 -0500

    Lint

--- os-app/_shared/WKCResponseParser/main.js
+++ os-app/_shared/WKCResponseParser/main.js
@@ -10,36 +10,36 @@
 	(factory((global.WKCResponseParser = global.WKCResponseParser || {})));
 }(this, (function (exports) { 'use strict';
 
-//_ WKCResponseParserArticlesForTwitterTimeline
+	//_ WKCResponseParserArticlesForTwitterTimeline
 
-exports.WKCResponseParserArticlesForTwitterTimeline = function(oldBody, newBody) {
-	if (typeof newBody !== 'string') {
-		throw new Error('WKCErrorInvalidInput');
-	}
+	exports.WKCResponseParserArticlesForTwitterTimeline = function(oldBody, newBody) {
+		if (typeof newBody !== 'string') {
+			throw new Error('WKCErrorInvalidInput');
+		}
 
-	const oldJSON = JSON.parse(oldBody || '[]');
+		const oldJSON = JSON.parse(oldBody || '[]');
 
-	const oldIDs = oldJSON.map(function (e) {
-		return e.id_str;
-	});
+		const oldIDs = oldJSON.map(function (e) {
+			return e.id_str;
+		});
 
-	const newJSON = JSON.parse(newBody);
+		const newJSON = JSON.parse(newBody);
 
-	if (!newJSON) {
-		return [];
-	}
+		if (!newJSON) {
+			return [];
+		}
 
-	return newJSON.filter(function(e) {
-		return oldIDs.indexOf(e.id_str) === -1;
-	}).map(function(e) {
-		return {
-			WKCArticleOriginalGUID: e.id_str,
-			WKCArticleOriginalURL: `https://twitter.com/${e.user.screen_name}/status/${e.id_str}`,
-			WKCArticlePublishDate: new Date(e.created_at),
-			WKCArticleBody: e.text,
-		};
-	});
-};
+		return newJSON.filter(function(e) {
+			return oldIDs.indexOf(e.id_str) === -1;
+		}).map(function(e) {
+			return {
+				WKCArticleOriginalGUID: e.id_str,
+				WKCArticleOriginalURL: `https://twitter.com/${e.user.screen_name}/status/${e.id_str}`,
+				WKCArticlePublishDate: new Date(e.created_at),
+				WKCArticleBody: e.text,
+			};
+		});
+	};
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
