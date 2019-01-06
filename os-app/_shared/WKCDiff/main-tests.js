3ddc458e6bfc63012f0c4b8b5f5273f02c85e8b9
Rosano Coutinho <pub@xeari.com>
2019-01-06 12:34:00 -0500

    Move _WKCDiffArticleBodyForStrings to library

---
+++ os-app/_shared/WKCDiff/main-tests.js
@@ -0,0 +1,73 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+const assert = require('assert');
+
+var mainModule = require('./main');
+
+const kTests = {
+	kTestsTextMultiline: function(count) {
+		return 'alfa bravo charlie delta echo foxtrot golf hotel indigo juliet kilo'.split(' ').slice(0, typeof count === 'undefined' ? Infinity : count).join('\n').concat('\n');
+	},
+};
+
+describe('WKCDiffHTMLForStrings', function testWKCDiffHTMLForStrings() {
+
+	it('throws error if param2 not string', function() {
+		assert.throws(function() {
+			mainModule.WKCDiffHTMLForStrings('alfa', null);
+		}, /WKCErrorInvalidInput/);
+	});
+
+	it('adds markup if param1 null', function() {
+		assert.strictEqual(mainModule.WKCDiffHTMLForStrings(null, 'alfa'), '{WKCDiffInsert}alfa{/WKCDiffInsert}');
+	});
+
+	it('returns identical if no change', function() {
+		assert.strictEqual(mainModule.WKCDiffHTMLForStrings('alfa', 'alfa'), 'alfa');
+	});
+
+	it('adds markup if character added', function() {
+		assert.strictEqual(mainModule.WKCDiffHTMLForStrings('alfa', 'alfax'), 'alfa{WKCDiffInsert}x{/WKCDiffInsert}');
+	});
+
+	it('adds markup if character removed', function() {
+		assert.strictEqual(mainModule.WKCDiffHTMLForStrings('alfa', 'alf'), 'alf{WKCDiffDelete}a{/WKCDiffDelete}');
+	});
+
+	it('adds markup if character changed', function() {
+		assert.strictEqual(mainModule.WKCDiffHTMLForStrings('alfa', 'alfo'), 'alf{WKCDiffDelete}a{/WKCDiffDelete}{WKCDiffInsert}o{/WKCDiffInsert}');
+	});
+
+	context('truncation', function() {
+
+		it('shows head if in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(4), kTests.kTestsTextMultiline(4).replace('delta', 'deltax')), kTests.kTestsTextMultiline(4).replace('delta', 'delta{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+		it('shows truncated head if not in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(5), kTests.kTestsTextMultiline(5).replace('echo', 'echox')), kTests.kTestsTextMultiline(5).replace('alfa', '…').replace('echo', 'echo{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+		it('shows tail if in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(4), kTests.kTestsTextMultiline(4).replace('alfa', 'alfax')), kTests.kTestsTextMultiline(4).replace('alfa', 'alfa{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+		it('shows truncated tail if not in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(5), kTests.kTestsTextMultiline(5).replace('alfa', 'alfax')), kTests.kTestsTextMultiline(5).replace('echo', '…').replace('alfa', 'alfa{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+		it('shows body if in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(8), kTests.kTestsTextMultiline(8).replace('alfa', 'alfax').replace('hotel', 'hotelx')), kTests.kTestsTextMultiline(8).replace('alfa', 'alfa{WKCDiffInsert}x{/WKCDiffInsert}').replace('hotel', 'hotel{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+		it('shows truncated body if not in range', function() {
+			assert.strictEqual(mainModule.WKCDiffHTMLForStrings(kTests.kTestsTextMultiline(9), kTests.kTestsTextMultiline(9).replace('alfa', 'alfax').replace('indigo', 'indigox')), kTests.kTestsTextMultiline(9).replace('echo', '…').replace('alfa', 'alfa{WKCDiffInsert}x{/WKCDiffInsert}').replace('indigo', 'indigo{WKCDiffInsert}x{/WKCDiffInsert}'));
+		});
+
+	});
+
+});
---
+++ os-app/_shared/WKCDiff/main.js
@@ -0,0 +1,116 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+// const diffPackage = require('diff');
+const diffPatchMatchPackage = require('diff-match-patch');
+
+//_ WKCDiffHTMLForStrings
+
+exports.WKCDiffHTMLForStrings = function(oldString, newString) {
+	if (typeof newString !== 'string') {
+		throw new Error('WKCErrorInvalidInput');
+	}
+
+	return (new diffPatchMatchPackage()).diff_main(oldString || '', newString).map(function(e) {
+		return {
+			added: e[0] === 1 ? true : undefined,
+			removed: e[0] === -1 ? true : undefined,
+			value: e[1],
+		};
+	}).map(function(e, index, collection) {
+		if (e.added) {
+			return [
+				'{WKCDiffInsert}',
+				e.value,
+				'{/WKCDiffInsert}',
+			].join('');
+		}
+
+		if (e.removed) {
+			return [
+				'{WKCDiffDelete}',
+				e.value,
+				'{/WKCDiffDelete}',
+			].join('');
+		}
+
+		if (e.value.split('\n').length <= 4) {
+			return e.value;
+		}
+
+		if (!index) {
+			return ['…'].concat(e.value.split('\n').slice(-4)).join('\n');
+		}
+
+		if (index === (collection.length - 1) && e.value.split('\n').length <= 5) {
+			return e.value;
+		}
+
+		if (index === (collection.length - 1)) {
+			return e.value.split('\n').slice(0, 4).concat(['…']).join('\n').concat('\n');
+		}
+
+		if (e.value.split('\n').length <= 8) {
+			return e.value;
+		}
+
+		return e.value.split('\n').slice(0, 4).concat(['…']).concat(e.value.split('\n').slice(-4)).join('\n');
+
+		return e.value;
+	}).join('');
+
+	var lineDiffs = diffPackage.diffLines(oldString || '', newString);
+
+	var truncateCallback = function(e, index, collection, ignoreFlag) {
+		const defaultValue = ignoreFlag ? '' : e.value;
+
+		if (e.added || e.removed) {
+			return defaultValue;
+		}
+
+		if (e.value.split('\n').length <= 4) {
+			return defaultValue;
+		}
+
+		if (!index) {
+			return ['…'].concat(e.value.split('\n').slice(-4)).join('\n');
+		}
+
+		if (index === (collection.length - 1)) {
+			return e.value.split('\n').slice(0, 3).concat(['…']).join('\n').concat('\n');
+		}
+
+		if (e.value.split('\n').length <= 7) {
+			return defaultValue;
+		}
+
+		return e.value.split('\n').slice(0, 3).concat(['…']).concat(e.value.split('\n').slice(-4)).join('\n');
+	};
+
+	return diffPackage.diffChars(lineDiffs.map(function(e, index, collection) {
+		return truncateCallback(e, index, collection, e.added);
+	}).join(''), lineDiffs.map(function(e, index, collection) {
+		return truncateCallback(e, index, collection, e.removed);
+	}).join('')).map(function(e) {
+		if (e.added === true) {
+			return [
+				'{WKCDiffInsert}',
+				e.value,
+				'{/WKCDiffInsert}',
+			].join('');
+		}
+
+		if (e.removed === true) {
+			return [
+				'{WKCDiffDelete}',
+				e.value,
+				'{/WKCDiffDelete}',
+			].join('');
+		}
+
+		return e.value;
+	}).join('');
+};
