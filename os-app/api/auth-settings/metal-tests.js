848aaa79b3a3d098dad438ad86095358e31b5f03
Rosano Coutinho <pub@xeari.com>
2019-01-31 19:48:56 -0500

    Spec WKCSettingsMetalProperty

--- os-app/api/auth-settings/metal-tests.js
+++ os-app/api/auth-settings/metal-tests.js
@@ -8,6 +8,7 @@ const assert = require('assert');
 
 var mainModule = require('./metal.js');
 var notesMetalLibrary = require('../auth-notes/metal.js');
+var notesActionLibrary = require('../auth-notes/action.js');
 
 const kTesting = {
 	StubValidNote: function() {
@@ -17,6 +18,32 @@ const kTesting = {
 	},
 };
 
+describe.only('WKCSettingsMetalProperty', function testWKCSettingsMetalProperty() {
+
+	it('rejects if param1 not string', function() {
+		assert.rejects(mainModule.WKCSettingsMetalProperty(WKCTestingMongoClient, null));
+	});
+
+	it('returns null if param1 not found', async function() {
+		assert.deepEqual(await mainModule.WKCSettingsMetalProperty(WKCTestingMongoClient, 'alfa'), null);
+	});
+
+	context('param2', function () {
+
+		it('returns value if undefined', async function() {
+			await mainModule.WKCSettingsMetalProperty(WKCTestingMongoClient, 'alfa', 'bravo');
+
+			assert.deepEqual(await mainModule.WKCSettingsMetalProperty(WKCTestingMongoClient, 'alfa'), 'bravo');
+		});
+
+		it('returns true and sets value', async function() {
+			assert.deepEqual(await mainModule.WKCSettingsMetalProperty(WKCTestingMongoClient, 'alfa', 'bravo'), true);
+		});
+		
+	});
+
+});
+
 describe.only('WKCSettingsMetalNotesLastPublicID', function testWKCSettingsMetalNotesLastPublicID() {
 
 	it('returns 0 if no published items', async function() {
@@ -24,7 +51,7 @@ describe.only('WKCSettingsMetalNotesLastPublicID', function testWKCSettingsMetal
 	});
 
 	it('returns 1 if published one', async function() {
-		let item = await notesMetalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, kTesting.StubValidNote());
+		await notesActionLibrary.WKCNotesActionPublish(WKCTestingMongoClient, (await notesMetalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, kTesting.StubValidNote())).WKCNoteID);
 
 		assert.deepEqual(await mainModule.WKCSettingsMetalNotesLastPublicID(WKCTestingMongoClient), 1);
 	});
--- os-app/api/auth-settings/metal.js
+++ os-app/api/auth-settings/metal.js
@@ -4,6 +4,55 @@
  * MIT Licensed
  */
 
+//_ _WKCSettingsMetalSet
+
+exports._WKCSettingsMetalSet = async function(databaseClient, param1, param2) {
+	if (typeof param1 !== 'string') {
+		return Promise.reject(new Error('WKCErrorInvalidInput'));
+	}
+
+	if (typeof param2 === 'undefined') {
+		return Promise.reject(new Error('WKCErrorInvalidInput'));
+	}
+
+	let outputData = (await databaseClient.db(process.env.WKC_SHARED_DATABASE_NAME).collection('wkc_settings').findOneAndUpdate({
+		WKCSettingsKey: param1,
+	}, {
+		'$set': {
+			WKCSettingValue: param2,
+		},
+	}, {
+		upsert: true,
+		returnOriginal: false,
+	})).value;
+
+	return Promise.resolve(true);
+};
+
+//_ _WKCSettingsMetalGet
+
+exports._WKCSettingsMetalGet = async function(databaseClient, inputData) {
+	if (typeof inputData !== 'string') {
+		return Promise.reject(new Error('WKCErrorInvalidInput'));
+	}
+
+	let outputData = await databaseClient.db(process.env.WKC_SHARED_DATABASE_NAME).collection('wkc_settings').findOne({
+		WKCSettingsKey: inputData,
+	});
+
+	return Promise.resolve(outputData ? outputData.WKCSettingValue : undefined);
+};
+
+//_ WKCSettingsMetalProperty
+
+exports.WKCSettingsMetalProperty = async function(databaseClient, param1, param2) {
+	if (typeof param2 === 'undefined') {
+		return await exports._WKCSettingsMetalGet(databaseClient, param1);
+	}
+
+	return await exports._WKCSettingsMetalSet(databaseClient, param1, param2);
+};
+
 //_ WKCSettingsMetalNotesLastPublicID
 
 exports.WKCSettingsMetalNotesLastPublicID = async function(databaseClient) {
