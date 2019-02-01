1d1f6981d6ca7447c8e254203d942b0a0fe1ffb3
Rosano Coutinho <pub@xeari.com>
2019-01-31 20:18:04 -0500

    Test multiple publish and delete

--- os-app/api/auth-notes/action-tests.js
+++ os-app/api/auth-notes/action-tests.js
@@ -43,7 +43,25 @@ describe.only('WKCNotesActionPublish', function testWKCNotesActionPublish() {
 
 		assert.deepEqual(items.map(function (e) {
 			return e.WKCNotePublicID;
-		}).sort(), ['1',	'2'].sort());
+		}).sort(), ['1','2'].sort());
+	});
+
+	it('returns WKCNote with updates if multiple published and deleted', async function() {
+		let itemCreated = await metalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, kTesting.StubValidNote());
+		let itemUpdated = await mainModule.WKCNotesActionPublish(WKCTestingMongoClient, itemCreated.WKCNoteID);
+		await metalLibrary.WKCNotesMetalDelete(WKCTestingMongoClient, itemCreated.WKCNoteID)
+
+		let itemCreated2 = await metalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, kTesting.StubValidNote());
+		let itemUpdated2 = await mainModule.WKCNotesActionPublish(WKCTestingMongoClient, itemCreated2.WKCNoteID);
+
+		await metalLibrary.WKCNotesMetalDelete(WKCTestingMongoClient, itemCreated2.WKCNoteID)
+		// await Promise.all(['alfa', 'bravo'].map(async function (e) {
+		// 	return await metalLibrary.WKCNotesMetalDelete(WKCTestingMongoClient, (await mainModule.WKCNotesActionPublish(WKCTestingMongoClient, (await metalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, Object.assign(kTesting.StubValidNote(), {
+		// 		WKCNoteBody: e,
+		// 	}))).WKCNoteID)).WKCNoteID);
+		// }));
+
+		assert.deepEqual((await mainModule.WKCNotesActionPublish(WKCTestingMongoClient, (await metalLibrary.WKCNotesMetalCreate(WKCTestingMongoClient, kTesting.StubValidNote())).WKCNoteID)).WKCNotePublicID, '3');
 	});
 
 });
