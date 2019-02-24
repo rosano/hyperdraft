9b6a02d896c523f27f68360fd4ba8dff0b3c2c7b
Rosano Coutinho <pub@xeari.com>
2019-02-24 09:35:00 +0100

    Rename behaviour

--- os-app/panel/open-write/ui-behaviour.js
+++ os-app/panel/open-write/ui-behaviour.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.RSPHomeBehaviour = global.RSPHomeBehaviour || {})));
+	(factory((global.WIKWriteBehaviour = global.WIKWriteBehaviour || {})));
 }(this, (function (exports) { 'use strict';
 
 	let moi = {};
