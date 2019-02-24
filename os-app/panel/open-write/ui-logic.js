7990846c2242ef51d220b0b992f28d4d8e01872c
Rosano Coutinho <pub@xeari.com>
2019-02-24 09:44:01 +0100

    Copy logic

---
+++ os-app/panel/open-write/ui-logic.js
@@ -0,0 +1,50 @@
+/*!
+ * wikiavec
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.WKCWriteLogic = global.WKCWriteLogic || {})));
+}(this, (function (exports) { 'use strict';
+
+	//_ WKCWriteLogicListSort
+
+	exports.WKCWriteLogicListSort = function (a, b) {
+		if (b.WKCNoteDateUpdated && a.WKCNoteDateUpdated) {
+			return b.WKCNoteDateUpdated > a.WKCNoteDateUpdated;
+		}
+
+		return b.WKCNoteDateCreated > a.WKCNoteDateCreated;
+	};
+
+	//_ WKCWriteLineObjectsFor
+
+	exports.WKCWriteLineObjectsFor = function (inputData) {
+		if (!Array.isArray(inputData)) {
+			throw new Error('WKCErrorInputInvalid');
+		}
+
+		return Object.values(inputData.reduce(function (coll, e) {
+			let key = (Object.keys(coll).pop() || -1);
+
+			if (!coll[key] || (coll[key].type !== e.type)) {
+				key += 1;
+				coll[key] = {
+					start: e.start,
+					type: e.type,
+				};
+			}
+
+			coll[key].end = e.end;
+			coll[key].string = (coll[key].string || '').concat(e.string);
+
+			return coll;
+		}, {}));
+	};
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
