d3e6b55fe8ca20f8e2b8af83c731a671f611f509
Rosano Coutinho <pub@xeari.com>
2019-02-24 18:18:49 +0100

    Enable service worker

---
+++ os-app/tech-sw/controller.js
@@ -0,0 +1,17 @@
+const kWKCServiceWorkerVersionID = Date.now();
+
+//_ OLSKControllerRoutes
+
+exports.OLSKControllerRoutes = function() {
+	return {
+		WKCServiceWorkerRoute: {
+			OLSKRoutePath: '/sw.js',
+			OLSKRouteMethod: 'get',
+			OLSKRouteFunction: function(req, res, next) {
+				return res.type('js').render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view.ejs'), {
+					WKCServiceWorkerVersionID: kWKCServiceWorkerVersionID,
+				});
+			},
+		},
+	};
+};
