d149d1cf05cb3e2e25fd04eaa9d1c21377e40922
Rosano Coutinho <pub@xeari.com>
2019-02-28 12:07:44 +0100

    Cl

--- os-app/panel/open-words/ui-behaviour.js
+++ os-app/panel/open-words/ui-behaviour.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.WIKWriteBehaviour = global.WIKWriteBehaviour || {})));
+	(factory((global.WKCWriteBehaviour = global.WKCWriteBehaviour || {})));
 }(this, (function (exports) { 'use strict';
 
 	let moi = {};
@@ -10,7 +10,7 @@
 
 	let WCKWriteBehaviourPropertyNoteObjects;
 	let WCKWriteBehaviourPropertyCurrentFilter;
-	var WCKWriteBehaviourPropertySelectedNote;
+	let WCKWriteBehaviourPropertySelectedNote;
 	let WCKWriteBehaviourPropertyEditor;
 
 	//# CONSTANTS
