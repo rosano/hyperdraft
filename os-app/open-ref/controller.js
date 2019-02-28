6ea07c7d62c960475e0436df4a4a92a0f0b0e683
Rosano Coutinho <pub@xeari.com>
2019-02-28 16:08:55 +0100

    Purge action definitions

--- os-app/open-ref/controller.js
+++ os-app/open-ref/controller.js
@@ -4,6 +4,9 @@
  * MIT Licensed
  */
 
+const notesActionLibrary = require('../api/auth-notes/action.js');
+const WKCParser = require('../_shared/WKCParser/main.js');
+
 //_ OLSKControllerRoutes
 
 exports.OLSKControllerRoutes = function() {
@@ -11,52 +14,41 @@ exports.OLSKControllerRoutes = function() {
 		WKCRouteHome: {
 			OLSKRoutePath: '/',
 			OLSKRouteMethod: 'get',
-			OLSKRouteFunction: exports.WKCActionHomeIndex,
+			OLSKRouteFunction: function(req, res, next) {
+				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
+					WKCNoteObject: {
+						WKCNoteDetectedTitle: res.locals.OLSKLocalized('WKCHomeTitle'),
+						WKCNoteDetectedBody: '',
+					},
+				});
+			},
 			OLSKRouteLanguages: ['en'],
 		},
 		WKCRouteRefsRead: {
 			OLSKRoutePath: '/:wkc_note_public_id(\\d+)',
 			OLSKRouteMethod: 'get',
-			OLSKRouteFunction: exports.WKCActionRefsRead,
+			OLSKRouteFunction: async function(req, res, next) {
+				let item = await notesActionLibrary.WKCNotesActionPublicRead(req.OLSKSharedConnectionFor('WKCSharedConnectionMongo').OLSKConnectionClient, req.params.wkc_note_public_id);
+
+				if ((item.message || '').match(/NotFound/)) {
+					return next();
+				}
+
+				item.WKCNoteDetectedTitle = WKCParser.WKCParserTitleForPlaintext(item.WKCNoteBody);
+				item.WKCNoteDetectedBody = WKCParser.WKCParserHTMLForPlaintext(WKCParser.WKCParserReplaceLinks(WKCParser.WKCParserBodyForPlaintext(item.WKCNoteBody), Object.entries(await notesActionLibrary.WKCNotesActionGetPublicLinks(req.OLSKSharedConnectionFor('WKCSharedConnectionMongo').OLSKConnectionClient)).map(function (e) {
+					return [e[0], `[${ e[0] }](${ res.locals.OLSKCanonicalFor('WKCRouteRefsRead', {
+						wkc_note_public_id: e[1],
+					}) })`]
+				}).reduce(function (coll, e) {
+					coll[e[0]] = e[1];
+
+					return coll;
+				}, {})));
+				
+				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
+					WKCNoteObject: item,
+				});
+			},
 		},
 	};
-};
-
-//_ WKCActionHomeIndex
-
-exports.WKCActionHomeIndex = function(req, res, next) {
-	return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
-		WKCNoteObject: {
-			WKCNoteDetectedTitle: res.locals.OLSKLocalized('WKCHomeTitle'),
-			WKCNoteDetectedBody: '',
-		},
-	});
-};
-
-//_ WKCActionRefsRead
-
-const notesActionLibrary = require('../api/auth-notes/action.js');
-const WKCParser = require('../_shared/WKCParser/main.js');
-
-exports.WKCActionRefsRead = async function(req, res, next) {
-	let item = await notesActionLibrary.WKCNotesActionPublicRead(req.OLSKSharedConnectionFor('WKCSharedConnectionMongo').OLSKConnectionClient, req.params.wkc_note_public_id);
-
-	if ((item.message || '').match(/NotFound/)) {
-		return next();
-	}
-
-	item.WKCNoteDetectedTitle = WKCParser.WKCParserTitleForPlaintext(item.WKCNoteBody);
-	item.WKCNoteDetectedBody = WKCParser.WKCParserHTMLForPlaintext(WKCParser.WKCParserReplaceLinks(WKCParser.WKCParserBodyForPlaintext(item.WKCNoteBody), Object.entries(await notesActionLibrary.WKCNotesActionGetPublicLinks(req.OLSKSharedConnectionFor('WKCSharedConnectionMongo').OLSKConnectionClient)).map(function (e) {
-		return [e[0], `[${ e[0] }](${ res.locals.OLSKCanonicalFor('WKCRouteRefsRead', {
-			wkc_note_public_id: e[1],
-		}) })`]
-	}).reduce(function (coll, e) {
-		coll[e[0]] = e[1];
-
-		return coll;
-	}, {})));
-	
-	return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
-		WKCNoteObject: item,
-	});
 };
