21c81077fcfcada47da7bfe04e23ead6a3e091ec
Rosano Coutinho <pub@xeari.com>
2019-07-15 10:17:22 -0400

    Setup editor

--- os-app/panel/open-compose/ModuleDetail.svelte
+++ os-app/panel/open-compose/ModuleDetail.svelte
@@ -1,21 +1,21 @@
 <script>
 import WKCNotesAction from '../../_shared/rs-modules/wkc_notes/action.js';
+import WKCVersionsAction from '../../_shared/rs-modules/wkc_versions/action.js';
+import WKCWriteLogic from '../open-write/ui-logic.js';
 
 import { storageClient, notesAll, noteSelected } from './persistence.js';
 
 let editorInstance;
 let editorInitializeValue = function () {
+	editorInstance.focus();
+
 	editorInstance.setValue($noteSelected.WKCNoteBody);
 	editorInstance.getDoc().clearHistory();
 }
 
 let _noteSelected;
 noteSelected.subscribe(function (val) {
-	if (val && (val !== _noteSelected)) {
-		document.querySelector('input').focus();
-
-		_noteSelected = val;
-	}
+	_noteSelected = val;
 
 	if (!val) {
 		editorInstance = null;
@@ -30,6 +30,35 @@ noteSelected.subscribe(function (val) {
 	editorInitializeValue();
 });
 
+function openTextObject (inputData) {
+	if (!!URLParse(inputData, {}).protocol) {
+		return window.open(inputData, '_blank');
+	}
+
+	let matches = inputData.match(/\[\[(.*)\]\]/);
+	if (!matches) {
+		return;
+	}
+
+	event.preventDefault();
+	
+	$filterText = matches.pop();
+};
+
+function openCursorObject (inputData) {
+	let cursor = editorInstance.getCursor();
+
+	let currentObject = WKCWriteLogic.WKCWriteLineObjectsFor(editorInstance.getLineTokens(cursor.line)).filter(function (e) {
+		return Math.max(e.start, Math.min(e.end, cursor.ch)) === cursor.ch;
+	}).shift();
+
+	if (!currentObject.type.match('link')) {
+		return;
+	}
+
+	openTextObject(currentObject.string);
+};
+
 let editorElement;
 import { afterUpdate } from 'svelte';
 afterUpdate(function () {
@@ -41,30 +70,96 @@ afterUpdate(function () {
 		return;
 	}
 
-	editorInstance = CodeMirror.fromTextArea(editorElement, {
-		mode: 'javascript',
+	(function setupEditor() {
+		editorInstance = CodeMirror.fromTextArea(editorElement, {
+			mode: {
+			  name: 'gfm',
+			  gitHubSpice: false,
+			  emoji: false,
+			},
+			
+			lineNumbers: false,
+			lineWrapping: true,
 
-		lineNumbers: true,
-		lineWrapping: true,
+			extraKeys: {
+			  Enter: 'newlineAndIndentContinueMarkdownList',
+			  'Cmd-Enter': openCursorObject,
+			  'Ctrl-Enter': openCursorObject,
+			  Esc: function () {
+			    return moi.kDefaultFocusNode().focus();
+			  },
+			  Tab: false,
+			},
 
-		placeholder: window.OLSKLocalized('WIKComposeListItemFormInputFunctionBodyPlaceholder'),
-		
-	  keyMap: 'sublime',
-	});
+			theme: 'wkc',
+
+			keyMap: 'sublime',
+		});
+
+		editorInstance.on('change', function (instance, changeObject) {
+			if (changeObject.origin === 'setValue') {
+				return;
+			}
+
+			Object.assign($noteSelected, {
+				WKCNoteBody: instance.getValue(),
+			}); // @DependancySvelteIgnoresMutableChanges
+
+			noteSave();
+		});
+
+		editorInstance.on('keydown', function (instance, event) {
+			if (!navigator.platform.match(/mac/i)) {
+				return;
+			}
+
+			if (!navigator.userAgent.match(/^((?!chrome|android).)*safari/i)) {
+				return;
+			}
+
+			if (!event.altKey || !event.ctrlKey) {
+				return;
+			}
+
+			let elements = document.querySelectorAll('[accesskey]');
+
+			if (!elements.length) {
+				return;
+			}
+
+			let match = [].slice.call(elements).filter(function (e) {
+				return e.getAttribute('accesskey') === event.key;
+			}).shift();
+
+			if (!match) {
+				return;
+			}
+
+			event.preventDefault();
+
+			setTimeout(function () {
+				match.focus();
+			});
+		});
+
+		editorInstance.on('mousedown', function (instance, event) {
+			if (!event.target.className.match('cm-link'))  {
+				return;
+			}
+
+			event.preventDefault();
+		});
+
+		document.querySelector('.CodeMirror').addEventListener('mouseup)', function (event) {
+			if (!event.target.className.match('cm-link'))  {
+				return;
+			}
+
+			openTextObject(event.target.textContent);
+		});
+	})();
 
 	editorInitializeValue();
-
-	editorInstance.on('change', function (instance, changeObject) {
-		if (changeObject.origin === 'setValue') {
-			return;
-		}
-
-		Object.assign($noteSelected, {
-			WKCNoteBody: instance.getValue(),
-		}); // @DependancySvelteIgnoresMutableChanges
-
-		noteSave();
-	});
 });
 
 let throttleMap = {};
@@ -109,13 +204,16 @@ async function noteDelete() {
 
 {#if $noteSelected}
 	<header class="WIKSharedToolbar">
-		<button on:click={ noteDelete } class="WKCSharedButtonNoStyle">{ window.OLSKLocalized('WIKComposeListItemToolbarDeleteButtonText') }</button>
+		<button on:click={ noteDelete } class="WKCSharedButtonNoStyle">{ window.OLSKLocalized('WKCWriteDetailToolbarDiscardButtonText') }</button>
 	</header>
+	<div class="EditorContainer">
+		<textarea bind:this={ editorElement }></textarea>
+	</div>
 {/if}
 
 {#if !$noteSelected}
 	<div class="PlaceholderContainer">
-		<span>{ window.OLSKLocalized('WIKComposeDetailPlaceholderText') }</span>
+		<span>{ window.OLSKLocalized('WKCWriteDetailPlaceholderText') }</span>
 	</div>
 {/if}
 
@@ -142,5 +240,46 @@ async function noteDelete() {
 	display: flex;
 	justify-content: center;
 	align-items: center;
+}
+
+.EditorContainer {
+	position:relative;
+
+	/* ContainerFlexboxChild */
+	flex-grow: 1;
+}
+
+.EditorContainer :global(.CodeMirror) {
+	height: 100%;
+
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+
+	font-family: inherit;
+}
+
+.EditorContainer :global(.CodeMirror-scroll) {
+	background: #f8f8f8;
+}
+
+.EditorContainer :global(.CodeMirror-lines) {
+	/* override defaults */
+	padding: 10px;
+}
+
+.EditorContainer :global(.CodeMirror pre) {
+	/* override defaults */
+	padding: 0;
+}
+
+.EditorContainer :global(.CodeMirror-line:nth-child(1)) {
+	font-weight: bold;
+}
+
+.EditorContainer :global(.cm-link:hover) {
+  cursor: pointer;
 }
 </style>
