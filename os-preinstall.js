9283a127acbf3a96618b083c2a85f4afe8e7bef4
Rosano Coutinho <pub@xeari.com>
2019-07-20 13:09:37 -0400

    Fix typo

--- os-preinstall.js
+++ os-preinstall.js
@@ -5,18 +5,16 @@
 		return;
 	}
 
-	try {
-		const OLSKDisk = require('OLSKDisk');
-	} catch (e) {
-		return console.warn('error requiring OLSKDisk');
-	}
-
 	(function (param1, param2) {
 		return require('glob').sync(`+(${ param1.join('|') })/`, {
 			matchBase: true,
 			cwd: param2,
 		}).forEach(function (e) {
-			OLSKDisk.OLSKDiskDeleteFolder(require('path').join(param2, e));
+			try {
+				require('OLSKDisk').OLSKDiskDeleteFolder(require('path').join(param2, e));
+			} catch (e) {
+				console.warn('error deleting folder');
+			}
 		});
 	})([
 		'OLSK*',
