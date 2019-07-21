ed4d6640daaa7d9f2024fd296d5147418b8b45e2
Rosano Coutinho <pub@xeari.com>
2019-07-21 17:50:39 -0400

    Rename PLUGIN_ALFA_SEARCH_REPLACE

--- os-app/_shared/common/global.js
+++ os-app/_shared/common/global.js
@@ -1,6 +1,6 @@
 import OLSKInternational from 'OLSKInternational';
 
-let localizationDictionary = JSON.parse(`{"PLUGIN_ALFA_SEARCH_REPLACE":"PLUGIN_ALFA_SEARCH_REPLACE"}`);
+let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
 
 export const OLSKLocalized = function(translationConstant) {
 	return OLSKInternational.OLSKInternationalLocalizedStringWithTranslationKeyAndTranslationDictionary(translationConstant, localizationDictionary[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
