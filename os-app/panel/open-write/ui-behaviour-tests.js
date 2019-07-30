ff2cbfcfcafedce25f352cd8bf8b7466bbf7a001
Rosano Coutinho <pub@xeari.com>
2019-07-30 12:28:39 -0400

    Lint

--- os-app/panel/open-write/ui-behaviour-tests.js
+++ os-app/panel/open-write/ui-behaviour-tests.js
@@ -5,72 +5,72 @@ const Browser = require('zombie');
 Browser.localhost(process.env.ZOMBIE_HOST, 3000);
 
 const kTesting = {
-  WKCWriteFilterInput: '#WIKDefaultFocusNode',
-  WKCWriteCreateButton: '#WKCWriteCreateButton',
+	WKCWriteFilterInput: '#WIKDefaultFocusNode',
+	WKCWriteCreateButton: '#WKCWriteCreateButton',
 
 	WKCWriteListItem: '.ListItem',
-  WKCWriteExportButton: '#WKCWriteExportButton',
+	WKCWriteExportButton: '#WKCWriteExportButton',
 
-  WKCWriteDetailPlaceholderContainer: '.PlaceholderContainer',
+	WKCWriteDetailPlaceholderContainer: '.PlaceholderContainer',
 
-  WKCWriteReloadButton: '#WKCWriteReloadButton',
+	WKCWriteReloadButton: '#WKCWriteReloadButton',
 };
 
 describe('WKCWriteBehaviourVisibilityStartup', function() {
 
-  const browser = new Browser();
+	const browser = new Browser();
 
-  before(function() {
-    return browser.visit('/panel/write');
-  });
+	before(function() {
+		return browser.visit('/panel/write');
+	});
 
-  it('includes elements', function() {
-    browser.assert.element(kTesting.WKCWriteFilterInput);
-    browser.assert.element(kTesting.WKCWriteCreateButton);
+	it('includes elements', function() {
+		browser.assert.element(kTesting.WKCWriteFilterInput);
+		browser.assert.element(kTesting.WKCWriteCreateButton);
 
-    browser.assert.element(kTesting.WKCWriteExportButton);
-    
-    browser.assert.element(kTesting.WKCWriteDetailPlaceholderContainer);
+		browser.assert.element(kTesting.WKCWriteExportButton);
+		
+		browser.assert.element(kTesting.WKCWriteDetailPlaceholderContainer);
 
-    browser.assert.element(kTesting.WKCWriteReloadButton);
-  });
+		browser.assert.element(kTesting.WKCWriteReloadButton);
+	});
 
-  it('excludes elements', function() {
-    browser.assert.elements(kTesting.WKCWriteListItem, 0);
-  });
+	it('excludes elements', function() {
+		browser.assert.elements(kTesting.WKCWriteListItem, 0);
+	});
 
 });
 
 describe('WKCWriteBehaviourLocalizationEN', function() {
 
-  const browser = new Browser();
+	const browser = new Browser();
 
-  before(function() {
-    return browser.visit('/panel/write');
-  });
+	before(function() {
+		return browser.visit('/panel/write');
+	});
 
-  it('localizes interface', function() {
-    deepEqual(browser.query(kTesting.WKCWriteCreateButton).title, 'Add note');
-  });
+	it('localizes interface', function() {
+		deepEqual(browser.query(kTesting.WKCWriteCreateButton).title, 'Add note');
+	});
 
 });
 
 describe('WKCWriteBehaviourInteraction', function() {
 
-  const browser = new Browser();
+	const browser = new Browser();
 
-  before(function() {
-    return browser.visit('/panel/write');
-  });
+	before(function() {
+		return browser.visit('/panel/write');
+	});
 
-  it('has no items', async function() {
-    browser.assert.elements(kTesting.WKCWriteListItem, 0);
-  });
+	it('has no items', async function() {
+		browser.assert.elements(kTesting.WKCWriteListItem, 0);
+	});
 
-  it('creates item', async function() {
-    browser.pressButton(kTesting.WKCWriteCreateButton);
-    await browser.wait({ element: kTesting.WKCWriteListItem });
-    browser.assert.elements(kTesting.WKCWriteListItem, 1);
-  });
+	it('creates item', async function() {
+		browser.pressButton(kTesting.WKCWriteCreateButton);
+		await browser.wait({ element: kTesting.WKCWriteListItem });
+		browser.assert.elements(kTesting.WKCWriteListItem, 1);
+	});
 
 });
