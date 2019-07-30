23fd07ba5b2a5413457971c4b1f203892124ef9e
Rosano Coutinho <pub@xeari.com>
2019-07-30 11:11:03 -0400

    Spec interaction test

--- os-app/panel/open-write/ui-behaviour-tests.js
+++ os-app/panel/open-write/ui-behaviour-tests.js
@@ -1,9 +1,12 @@
+import { throws, deepEqual } from 'assert';
+
 const Browser = require('zombie');
 
 Browser.localhost(process.env.ZOMBIE_HOST, 3000);
 
 const kTesting = {
 	WKCWriteCreateButton: '#WKCWriteCreateButton',
+	WKCWriteListItem: '.ListItem',
 };
 
 describe('WKCWriteBehaviourVisibility', function() {
@@ -28,8 +31,28 @@ describe('WKCWriteBehaviourLocalizationEN', function() {
     return browser.visit('/panel/write');
   });
 
-  it('contains create button', function() {
-    browser.assert.attribute(kTesting.WKCWriteCreateButton, 'title', 'Add note');
+  it('localizes interface', function() {
+    deepEqual(browser.query(kTesting.WKCWriteCreateButton).title, 'Add note');
+  });
+
+});
+
+describe('WKCWriteBehaviourInteraction', function() {
+
+  const browser = new Browser();
+
+  before(function() {
+    return browser.visit('/panel/write');
+  });
+
+  it('has no items', async function() {
+    browser.assert.elements(kTesting.WKCWriteListItem, 0);
+  });
+
+  it('creates item', async function() {
+    browser.pressButton(kTesting.WKCWriteCreateButton);
+    await browser.wait({ element: kTesting.WKCWriteListItem });
+    browser.assert.elements(kTesting.WKCWriteListItem, 1);
   });
 
 });
