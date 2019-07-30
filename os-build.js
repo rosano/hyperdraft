eed6e7f666ef6e472f4f57ee6719e8dd7ae31c32
Rosano Coutinho <pub@xeari.com>
2019-07-30 09:35:21 -0400

    Hotfix ULID for browser testing

--- os-build.js
+++ os-build.js
@@ -1,3 +1,22 @@
+//# ROCOHotfixULIDForBrowserTesting
+
+(function ROCOHotfixULIDForBrowserTesting() {
+	let filePath = './node_modules/ulid/dist/index.esm.js';
+	require('fs').writeFileSync(filePath, require('fs')
+		.readFileSync(filePath, 'utf8')
+		.replace(
+			`console.error("secure crypto unusable, falling back to insecure Math.random()!");`,
+			'// console.error("secure crypto unusable, falling back to insecure Math.random()!");')
+		.replace(
+			`var ulid = factory();`,
+			`// var ulid = factory();`)
+		.replace(
+			`export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory, ulid };`,
+			`// export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory, ulid };
+export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory };`)
+		)
+})();
+
 //# OLSKStartExternalAssets
 
 (function OLSKStartExternalAssets() {
