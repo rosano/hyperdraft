5cc47a96a8abea97faf25f39088774af814c1efe
Rosano Coutinho <pub@xeari.com>
2019-08-06 16:33:35 -0400

    Spec long body

--- os-app/panel/open-write/ui-behaviour-tests.js
+++ os-app/panel/open-write/ui-behaviour-tests.js
@@ -236,6 +236,15 @@ describe('WKCWriteUITestLanguage', function testLanguage() {
 				deepEqual(browser.query(WKCWriteListItemAccessibilitySummary).textContent, 'alfa bravo charlie delta echo foxtrot golf hotel juliet…');
 				deepEqual(browser.query(WKCWriteListItemTitle).textContent, 'alfa bravo charlie delta echo foxtrot golf hotel juliet');
 				deepEqual(browser.query(WKCWriteListItemSnippet).textContent, '');
+			});
+
+			it('on edit long body', async function() {
+				browser.fill(WKCWriteEditorDebugInput, '\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+				await browser.wait({ element: WKCWriteListItem });
+
+				deepEqual(browser.query(WKCWriteListItemAccessibilitySummary).textContent, '');
+				deepEqual(browser.query(WKCWriteListItemTitle).textContent, '');
+				deepEqual(browser.query(WKCWriteListItemSnippet).textContent, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the…');
 
 				browser.fill(WKCWriteEditorDebugInput, 'alfa\nbravo');
 			});
