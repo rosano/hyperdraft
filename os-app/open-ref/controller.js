c6bf57bc6f7866d3b3695114e8159d1e5a8a10f4
Rosano Coutinho <pub@xeari.com>
2019-09-20 16:35:10 -0400

    Re-enable ref read

--- os-app/open-ref/controller.js
+++ os-app/open-ref/controller.js
@@ -1,12 +1,11 @@
-return;
+const { WKCRefStorageClient } = require('./client.js');
+const storageClient = WKCRefStorageClient();
 
-const storageClient = require('../_shared/WKCStorageClient/storage.js').WKCStorageClientForChangeDelegateMap({
-	wkc_notes: null,
-});
+const kWKCRefCacheKey = 'open-ref';
 
 //_ OLSKControllerSharedConnections
 
-exports.OLSKControllerSharedConnectionsx = function() {
+exports.OLSKControllerSharedConnections = function() {
 	return {
 		WKCSharedConnectionRS: {
 			OLSKConnectionInitializer: function(olskCallback) {
@@ -33,15 +32,15 @@ exports.OLSKControllerSharedConnectionsx = function() {
 
 //_ OLSKControllerSharedMiddlewares
 
-exports.OLSKControllerSharedMiddlewaresx = function() {
+exports.OLSKControllerSharedMiddlewares = function() {
 	return {
-		WKCSharedMiddlewareEnsureConnection: exports.WKCSharedMiddlewareEnsureConnectionx,
+		WKCSharedMiddlewareEnsureConnection: exports.WKCSharedMiddlewareEnsureConnection,
 	};
 };
 
 //_ WKCSharedMiddlewareEnsureConnection
 
-exports.WKCSharedMiddlewareEnsureConnectionx = function(req, res, next) {
+exports.WKCSharedMiddlewareEnsureConnection = function(req, res, next) {
 	if (!req.OLSKSharedConnectionFor('WKCSharedConnectionRS').OLSKConnectionAttempted) {
 		return next(new Error('WKCErrorConnectionNotAttempted'));
 	}
@@ -51,6 +50,29 @@ exports.WKCSharedMiddlewareEnsureConnectionx = function(req, res, next) {
 	}
 
 	return next();
+};
+
+//_ OLSKControllerTasks
+
+exports.OLSKControllerTasks = function () {
+	return [
+		{
+			OLSKTaskName: 'WKCRefCacheTask',
+			OLSKTaskFireTimeInterval: 60 * 60,
+			OLSKTaskShouldBePerformed: function () {
+				return true;
+			},
+			OLSKTaskCallback: async function (callbackInput) {
+				callbackInput.OLSKLive.OLSKCacheWriteWithCacheKeyAndCacheObject(kWKCRefCacheKey, await exports.RCSRefFetchPublicNotes());
+			},
+		},
+	];
+};
+
+exports.RCSRefFetchPublicNotes = async function () {
+	return Object.values(await storageClient.wkc_notes.listObjects()).filter(function (e) {
+		return e.WKCNotePublishStatusIsPublished;
+	});
 };
 
 //_ OLSKControllerRoutes
@@ -74,18 +96,36 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/:wkc_note_public_id(\\d+)',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: async function(req, res, next) {
-				return next();
-				const WKCNotesAction = require('../_shared/rs-modules/wkc_notes/action.js');
-				const WKCParser = require('../_shared/WKCParser/main.js');
+				let publicNotes = req.OLSKCacheReadForCacheKey(kWKCRefCacheKey);
 
-				let item = await WKCNotesAction.WKCNotesActionPublicRead(req.OLSKSharedConnectionFor('WKCSharedConnectionRS').OLSKConnectionClient, req.params.wkc_note_public_id);
+				if (!publicNotes) {
+					publicNotes = await exports.RCSRefFetchPublicNotes()
 
-				if ((item.message || '').match(/NotFound/)) {
+					req.OLSKCacheWriteWithCacheKeyAndCacheObject(kWKCRefCacheKey, publicNotes);
+				};
+
+				let item = publicNotes.filter(function (e) {
+					return e.WKCNotePublicID === req.params.wkc_note_public_id;
+				}).shift();
+
+				if (!item) {
 					return next();
 				}
 
+				const WKCParser = require('../_shared/WKCParser/main.js');
+
+				const publicLinks = publicNotes.map(function (e) {
+					return [WKCParser.WKCParserTitleForPlaintext(e.WKCNoteBody), e.WKCNotePublicID];
+				}).reduce(function (coll, [key, val]) {
+					if (typeof coll[key] === 'undefined') {
+						coll[key] = val;
+					}
+
+					return coll;
+				}, {});
+				
 				item.WKCNoteDetectedTitle = WKCParser.WKCParserTitleForPlaintext(item.WKCNoteBody);
-				item.WKCNoteDetectedBody = WKCParser.WKCParserHTMLForPlaintext(WKCParser.WKCParserReplaceLinks(WKCParser.WKCParserBodyForPlaintext(item.WKCNoteBody), Object.entries(await WKCNotesAction.WKCNotesActionGetPublicLinks(req.OLSKSharedConnectionFor('WKCSharedConnectionRS').OLSKConnectionClient)).map(function (e) {
+				item.WKCNoteDetectedBody = WKCParser.WKCParserHTMLForPlaintext(WKCParser.WKCParserReplaceLinks(WKCParser.WKCParserBodyForPlaintext(item.WKCNoteBody), Object.entries(publicLinks).map(function (e) {
 					return [e[0], `[${ e[0] }](${ res.locals.OLSKCanonicalFor('WKCRouteRefsRead', {
 						wkc_note_public_id: e[1],
 					}) })`];
@@ -95,7 +135,7 @@ exports.OLSKControllerRoutes = function() {
 					return coll;
 				}, {})));
 				
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
+				return res.render(require('path').join(__dirname, 'view'), {
 					WKCNoteObject: item,
 				});
 			},
