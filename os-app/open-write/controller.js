533e89a506cccf42ab358ca9c0f34ee0ded734ba
Rosano Coutinho <pub@xeari.com>
2019-12-05 12:37:44 -0500

    Rename view

--- os-app/open-write/controller.js
+++ os-app/open-write/controller.js
@@ -6,7 +6,7 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/panel/write',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function (req, res, next) {
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {});
+				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'ui-view.ejs'), {});
 			},
 			OLSKRouteLanguages: ['en'],
 			OLSKRouteMiddlewares: [
