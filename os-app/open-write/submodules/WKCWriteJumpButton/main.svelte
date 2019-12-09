e91f14c8f55e7f8f8050285317389e06e58ff3a9
Rosano Coutinho <pub@xeari.com>
2019-12-09 10:30:49 -0500

    Cleanup jump button

--- os-app/open-write/submodules/WKCWriteJumpButton/main.svelte
+++ os-app/open-write/submodules/WKCWriteJumpButton/main.svelte
@@ -2,25 +2,40 @@
 export let WKCWriteJumpButtonRecipes = [];
 export let WKCWriteJumpButtonDispatchComplete = null;
 
-function interfaceButtonDidClick() {
-	setTimeout(function () {
+import OLSKInternational from 'OLSKInternational';
+const OLSKLocalized = function(translationConstant) {
+	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`)[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
+};
+
+const mod = {
+
+	// INTERFACE
+	
+	InterfaceButtonDidClick() {
+		setTimeout(mod.ControlLaunchlet);
+	},
+
+	InterfaceWindowDidKeydown () {
+		if (event.ctrlKey && event.key === 'r' && WKCWriteJumpButtonRecipes.length) {
+			return ControlLaunchlet();
+		}
+	},
+
+	// CONTROL
+
+	ControlLaunchlet () {
 		window.Launchlet.LCHSingletonCreate({
 			LCHOptionRecipes: WKCWriteJumpButtonRecipes,
 			LCHOptionMode: window.Launchlet.LCHModePreview,
 			LCHOptionCompletionHandler: WKCWriteJumpButtonDispatchComplete,
 		});
-	});
-}
+	},
 
-function interfaceDidReceiveKeydown() {
-	if (event.ctrlKey && event.key === 'r' && WKCWriteJumpButtonRecipes.length) {
-		return interfaceButtonDidClick();
-	}
-}
+};
 </script>
-<svelte:window on:keydown={ interfaceDidReceiveKeydown }/>
+<svelte:window on:keydown={ mod.InterfaceWindowDidKeydown }/>
 
-<button on:click={ interfaceButtonDidClick  } class="WKCWriteJumpButton OLSKToolbarButton OLSKLayoutElementTappable OLSKLayoutButtonNoStyle" disabled={ !WKCWriteJumpButtonRecipes.length } accesskey="r" style="background-image: url('/panel/_shared/ui-assets/wIKWriteJump.svg')" title="Jump" tabindex="-1"></button>
+<button on:click={ mod.InterfaceButtonDidClick  } class="WKCWriteJumpButton OLSKToolbarButton OLSKLayoutElementTappable OLSKLayoutButtonNoStyle" disabled={ !WKCWriteJumpButtonRecipes.length } accesskey="r" style="background-image: url('/panel/_shared/ui-assets/wIKWriteJump.svg')" title={ OLSKLocalized('WKCWriteJumpButtonText') } tabindex="-1"></button>
 
 <style>
 </style>
