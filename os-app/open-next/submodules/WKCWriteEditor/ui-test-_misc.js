d3e6b8d42fa7996d6b5f7f474a5526ce948fc8e8
Rosano Coutinho <pub@xeari.com>
2019-12-09 11:43:11 -0500

    Tidy tests

--- os-app/open-next/submodules/WKCWriteEditor/ui-test-_misc.js
+++ os-app/open-next/submodules/WKCWriteEditor/ui-test-_misc.js
@@ -10,62 +10,47 @@ describe('WKCWriteEditor_Misc', function () {
 		});
 	});
 
-	describe('WKCWriteEditorFieldDebug', function() {
-		
-		it('binds WKCWriteEditorSetDocument', function () {
-			browser.assert.input(WKCWriteEditorFieldDebug, 'alfa');
-		});
-
-		context('input', function () {
-
-			before(function () {
-				browser.fill(WKCWriteEditorFieldDebug, 'bravo');
-			});
-
-			it('sends WKCWriteEditorDispatchUpdate', function () {
-				browser.assert.text('#TestWKCWriteEditorDispatchUpdate', '1');
-				browser.assert.text('#TestWKCWriteEditorDispatchUpdateData', 'bravo');
-			});
-		
-		});
-
+	it('binds WKCWriteEditorSetDocument', function () {
+		browser.assert.input(WKCWriteEditorFieldDebug, 'alfa');
 	});
+
+	it.skip('binds WKCWriteEditorSetDocument', function () {
+		browser.assert.input('.CodeMirror', 'alfa');
+	});
+
+	context('input', function () {
+
+		before(function () {
+			browser.assert.text('#TestWKCWriteEditorDispatchUpdate', '0');
+			browser.assert.text('#TestWKCWriteEditorDispatchUpdateData', 'undefined');
+		});
+
+		before(function () {
+			browser.fill(WKCWriteEditorFieldDebug, 'bravo');
+			// browser.fill('.CodeMirror', 'bravo');
+		});
+
+		it('sends WKCWriteEditorDispatchUpdate', function () {
+			browser.assert.text('#TestWKCWriteEditorDispatchUpdate', '1');
+			browser.assert.text('#TestWKCWriteEditorDispatchUpdateData', 'bravo');
+		});
 	
-	describe.skip('CodeMirror', function() {
+	});
+
+	context.skip('WKCWriteEditorFocus', function () {
+
+		before(function () {
+			browser.assert.hasNoClass('.CodeMirror', 'CodeMirror-focused');
+		});
+
+		before(function () {
+			browser.pressButton('#TestWKCWriteEditorFocus');
+		});
+
+		it('classes CodeMirror-focused', function () {
+			browser.assert.hasClass('.CodeMirror', 'CodeMirror-focused');
+		});
 		
-		it('binds WKCWriteEditorSetDocument', function () {
-			browser.assert.input('.CodeMirror', 'alfa');
-		});
-
-		context('WKCWriteEditorFocus', function () {
-
-			before(function () {
-				browser.assert.hasNoClass('.CodeMirror', 'CodeMirror-focused');
-			});
-
-			before(function () {
-				browser.pressButton('#TestWKCWriteEditorFocus');
-			});
-
-			it('classes CodeMirror-focused', function () {
-				browser.assert.hasClass('.CodeMirror', 'CodeMirror-focused');
-			});
-			
-		});
-
-		context('input', function () {
-
-			before(function () {
-				browser.fill('.CodeMirror', 'bravo');
-			});
-
-			it('sends WKCWriteEditorDispatchUpdate', function () {
-				browser.assert.text('#TestWKCWriteEditorDispatchUpdate', '1');
-				browser.assert.text('#TestWKCWriteEditorDispatchUpdateData', 'bravo');
-			});
-		
-		});
-
 	});
 
 });
