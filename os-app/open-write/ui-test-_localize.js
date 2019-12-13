b70e68186b4bf5ea948a8a669057d79abb8357a3
Rosano Coutinho <pub@xeari.com>
2019-12-13 12:21:07 -0500

    Replace OLSKTestingStringWithFormat

--- os-app/open-write/ui-test-_localize.js
+++ os-app/open-write/ui-test-_localize.js
@@ -2,7 +2,7 @@ import { deepEqual } from 'assert';
 
 const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-const uStringWithFormat = OLSKTestingStringWithFormat;
+const uStringWithFormat = require('OLSKString').OLSKStringWithFormat;
 
 kDefaultRoute.OLSKRouteLanguages.forEach(function (languageCode) {
 
