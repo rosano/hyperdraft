328933dacccda96dfa8cc036db0b21d942cd6972
Rosano Coutinho <pub@xeari.com>
2019-12-26 10:01:11 -0500

    Pass client key to view

--- os-app/open-write/controller.js
+++ os-app/open-write/controller.js
@@ -4,11 +4,15 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'KVCWriteRoute',
 		OLSKRouteFunction (req, res, next) {
-			return res.render(require('path').join(__dirname, 'ui-view.ejs'));
+			return res.render(require('path').join(__dirname, 'ui-view.ejs'), {
+				KVCDropboxAppKey: Buffer.from(process.env.KVC_DROPBOX_APP_KEY).toString('base64'),
+				KVCGoogleClientKey: Buffer.from(process.env.KVC_GOOGLE_CLIENT_KEY).toString('base64'),
+			});
 		},
 		OLSKRouteLanguages: ['en', 'fr', 'es'],
 		OLSKRouteMiddlewares: [
 			'KVCSharedDropboxAppKeyGuardMiddleware',
+			'KVCSharedGoogleClientKeyGuardMiddleware',
 			'KVCSharedDonateLinkGuardMiddleware',
 		],
 		_OLSKRouteSkipLanguageRedirect: true,
