bc9dbd99580a58ffc69a4d30283ebcc9b911df08
Rosano Coutinho <pub@xeari.com>
2020-06-22 12:57:10 -0300

    Spec sort

---
+++ os-app/open-write/ui-test-sort.js
@@ -0,0 +1,61 @@
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
+
+describe('KVCWrite_Sort', function () {	
+
+	before(function() {
+		return browser.OLSKVisit(kDefaultRoute);
+	});
+
+	before(function () {
+		return browser.pressButton('.KVCWriteMasterCreateButton');
+	});
+
+	before(function () {
+		browser.fill('.KVCWriteInputFieldDebug', 'alfa');
+	});
+
+	before(function () {
+		return browser.pressButton('.KVCWriteMasterCreateButton');
+	});
+
+	before(function () {
+		browser.fill('.KVCWriteInputFieldDebug', 'bravo');
+	});
+
+	before(function () {
+		return browser.pressButton('.KVCWriteMasterCreateButton');
+	});
+
+	before(function () {
+		browser.fill('.KVCWriteInputFieldDebug', 'charlie');
+	});
+
+	describe('update', function test_update () {
+
+		before(function () {
+			return browser.click('.OLSKResultsListItem:nth-child(3)');
+		});
+
+		before(function () {
+			browser.fill('.KVCWriteInputFieldDebug', 'alfa2');
+		});
+
+		it('skips sort', function () {
+			browser.assert.text('.OLSKResultsListItem', 'charlie bravo alfa2');
+		});
+
+	});
+
+	describe('unselect', function test_unselect () {
+
+		before(function () {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'Escape');
+		});
+
+		it('sorts list', function () {
+			browser.assert.text('.OLSKResultsListItem', 'alfa2 charlie bravo');
+		});
+
+	});
+
+});
