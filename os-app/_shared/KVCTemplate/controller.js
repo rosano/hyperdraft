68e554b0a8b6c944a7857ccf5e563a7c9dc8a751
Rosano Coutinho <pub@xeari.com>
2020-07-17 13:24:33 -0300

    Re-compile template for nodejs

--- os-app/_shared/KVCTemplate/controller.js
+++ os-app/_shared/KVCTemplate/controller.js
@@ -4,7 +4,9 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'KVCTemplateStubRoute',
 		OLSKRouteFunction (req, res, next) {
-			return res.send(require('./main.js').KVCTemplateViewDefault(res.locals.OLSKLocalized));
+			const filePath = require('path').join(__dirname, '__compiled/main.js');
+			require('OLSKDisk').OLSKDiskWriteFile(filePath, require('OLSKDisk').OLSKDiskReadFile(require('path').join(__dirname, 'main.js')).replace(`import * as OLSKStringPackage from 'OLSKString'`, `const OLSKStringPackage = require('OLSKString')`).replace('export default mod;', 'Object.assign(exports, mod);'));
+			return res.send(require(filePath).KVCTemplateViewDefault(res.locals.OLSKLocalized));
 		},
 		OLSKRouteLanguages: ['en', 'fr', 'es'],
 	}];
