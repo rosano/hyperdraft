1a3712fa4ac8cef0143e142dec217e7e0d779bd7
Rosano Coutinho <pub@xeari.com>
2020-12-05 11:11:40 -0300

    Name OLSKRemoteStorageRecipes params

--- os-app/open-write/main.svelte
+++ os-app/open-write/main.svelte
@@ -332,7 +332,13 @@ const mod = {
 			ParamSpecUI: OLSK_SPEC_UI(),
 		}));
 
-		outputData.push(...OLSKRemoteStorage.OLSKRemoteStorageRecipes(window, mod._ValueOLSKRemoteStorage, OLSKLocalized, OLSK_SPEC_UI()));
+		outputData.push(...OLSKRemoteStorage.OLSKRemoteStorageRecipes({
+			ParamWindow: window,
+			ParamStorage: mod._ValueOLSKRemoteStorage,
+			OLSKLocalized: OLSKLocalized,
+			ParamMod: mod,
+			ParamSpecUI: OLSK_SPEC_UI(),
+		});
 		outputData.push(...OLSKServiceWorker.OLSKServiceWorkerRecipes(window, mod.DataNavigator(), OLSKLocalized, OLSK_SPEC_UI()));
 
 		if (mod.KVCWriteDetailInstance) {
