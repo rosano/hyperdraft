f0dfeadb0c30c2ce05deb961221086f4afc2a417
Rosano Coutinho <pub@xeari.com>
2021-01-03 07:56:44 -0300

    Fix typo

--- os-app/_shared/KVCTemplate/controller.js
+++ os-app/_shared/KVCTemplate/controller.js
@@ -5,7 +5,15 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteSignature: 'KVCTemplateStubRoute',
 		OLSKRouteFunction (req, res, next) {
 			const filePath = require('path').join(__dirname, '__compiled/main.js');
-			require('OLSKDisk').OLSKDiskWriteFile(filePath, require('OLSKDisk').OLSKDiskReadFile(require('path').join(__dirname, 'main.js')).replace(`import OLSKString from 'OLSKString'`, `const OLSKString = require('OLSKString')`).replace('export default mod;', 'Object.assign(exports, mod);'));
+			require('OLSKDisk').OLSKDiskWriteFile(filePath, Object.entries({
+				[`import OLSKString from 'OLSKString'`]: `const OLSKString = require('OLSKString')`,
+				[`import marked from 'marked'`]: `const marked = require('marked')`,
+				[`import escapeRegExp from 'escape-string-regexp'`]: `const escapeRegExp = require('escape-string-regexp')`,
+				'export default mod;': 'Object.assign(exports, mod);',
+			}).reduce(function (coll, [search, replace]) {
+				return require('OLSKString').OLSKStringPatch(
+					coll, search, replace);
+			}, require('OLSKDisk').OLSKDiskReadFile(require('path').join(__dirname, 'main.js'))));
 			return res.send(require(filePath).KVCTemplateViewDefault(res.locals.OLSKLocalized));
 		},
 		OLSKRouteLanguageCodes: ['en', 'fr', 'es'],
