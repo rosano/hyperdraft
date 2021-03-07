f6d5dd71f6b1a9795cbec7f8ab92dda6aa0aabb5
Rosano Coutinho <pub@xeari.com>
2021-03-07 10:29:38 -0300

    Migrate between protocols

--- os-app/open-write/main.svelte
+++ os-app/open-write/main.svelte
@@ -1267,6 +1267,19 @@ const mod = {
 	},
 
 	async SetupValueNotesAll() {
+		if (zerodatawrap.ZDRPreferenceProtocolMigrate()) {
+			const client = await mod.DataStorageClient(zerodatawrap.ZDRPreferenceProtocolMigrate());
+
+			await Promise.all((await client.App.ZDRStoragePathsRecursive('/')).map(async function (e) {
+				await mod._ValueZDRWrap.App.ZDRStorageWriteObject(e, await client.App.ZDRStorageReadObject(e));
+				await client.App.ZDRStorageDeleteFile(e);
+			}));
+
+			zerodatawrap.ZDRPreferenceProtocolMigrateClear();
+
+			client.ZDRCloudDisconnect();
+		};
+
 		const items = await mod._ValueZDRWrap.App.KVCNote.KVCNoteList();
 
 		mod.ValueArchivedCount(items);
