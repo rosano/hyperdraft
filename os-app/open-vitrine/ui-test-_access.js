436347e958c472ac1468169a10fa8fb95d92b025
Rosano Coutinho <pub@xeari.com>
2021-06-30 19:10:00 -0300

    Integrate ROCOEphemerataLink

--- os-app/open-vitrine/ui-test-_access.js
+++ os-app/open-vitrine/ui-test-_access.js
@@ -99,7 +99,11 @@ describe('KVCVitrine_Access', function () {
 		browser.assert.elements(KVCVitrineSupportBlurb, 1);
 	});
 
-	it('shows SWARLink', function () {
+	it('shows ROCOEphemerataLink', function () {
+		browser.assert.elements('.ROCOEphemerataLink', 1);
+	});
+
+	it('shows SWARLink', function() {
 		browser.assert.elements('.SWARLink', 1);
 	});
 
