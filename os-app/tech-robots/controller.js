3d17f22515dc48c4f665d6163d3fb6421a373f68
Rosano Coutinho <pub@xeari.com>
2021-08-30 11:06:02 -0400

    Spec guide in robots

--- os-app/tech-robots/controller.js
+++ os-app/tech-robots/controller.js
@@ -6,6 +6,7 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteFunction (req, res, next) {
 			return res.send(require('OLSKRobots').OLSKRobotsTXT([
 				'KVCVitrineRoute',
+				'KVCGuideRoute',
 			].reduce(function (coll, item) {
 				return coll.concat(res.locals.OLSKCanonical(item)).concat((res.locals.OLSKRouteObjectFor(item).OLSKRouteLanguageCodes || []).map(function (e) {
 					return res.locals.OLSKCanonicalLocalized(item, {
--- os-app/tech-robots/ui-test-_misc.js
+++ os-app/tech-robots/ui-test-_misc.js
@@ -5,6 +5,7 @@ describe('WKCRobots_Misc', function () {
 	it('sends text', async function () {
 		browser.assert.deepEqual(await (await browser.fetch('http://loc.tests' + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
 				require('../open-vitrine/controller.js').OLSKControllerRoutes().shift(),
+				require('../open-guide/controller.js').OLSKControllerRoutes().shift(),
 			].reduce(function (coll, item) {
 				return coll.concat(OLSKTestingCanonical(item)).concat((item.OLSKRouteLanguageCodes || []).map(function (e) {
 					return OLSKTestingCanonical(item, {
