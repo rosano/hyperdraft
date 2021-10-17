fd3b1afc3218e7c82593ad963711d34c751de6be
Rosano Coutinho <pub@xeari.com>
2021-10-16 21:46:29 -0400

    Hyphenate text basename

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -5,7 +5,7 @@ exports.OLSKControllerRoutes = function () {
 		OLSKRouteSignature: 'KVCGuideRoute',
 		OLSKRouteFunction(req, res, next) {
 			return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), {
-				KVCGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), {
+				KVCGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text-${ res.locals.OLSKSharedPageCurrentLanguage }.md`), {
 					KVCWriteRoute: res.locals.OLSKCanonical('KVCWriteRoute'),
 				}),
 			});
