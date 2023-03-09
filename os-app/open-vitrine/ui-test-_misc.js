0b68e01505f9d456f377be29298546067ced4e37
Rosano Coutinho <pub@xeari.com>
2023-03-09 15:31:09 +0700

    Use project name in bulletin

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -169,7 +169,7 @@ describe('KVCVitrine_Misc', function () {
 	describe('ROCOGazette', function test_ROCOGazette () {
 
 		it('sets ROCOBulletinProject', function () {
-			browser.assert.attribute('.ROCOBulletinProjectField', 'value', process.env.ROCO_SHARED_PROJECT_ID);
+			browser.assert.attribute('.ROCOBulletinProjectField', 'value', 'Hyperdraft');
 		});
 
 	});
