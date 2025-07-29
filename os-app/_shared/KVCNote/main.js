3dd20b908261f6acdde9061d73f3b4e4ebeec57d
Rosano Coutinho <github.com@xeari.com>
2025-07-29 10:22:53 +0100

    update ulid api

--- os-app/_shared/KVCNote/main.js
+++ os-app/_shared/KVCNote/main.js
@@ -1,5 +1,5 @@
-import { factory } from 'ulid';
-const uniqueID = factory();
+import { ulid } from 'ulid';
+const uniqueID = ulid();
 import OLSKRemoteStorage from 'OLSKRemoteStorage';
 import KVCTemplate from '../KVCTemplate/main.js';
 
