9825eea74d7d508ed536dc7efeeb58c92e731472
Rosano Coutinho <github.com@xeari.com>
2025-09-09 10:32:10 +0100

    fission offline

--- os-app/open-write/main.svelte
+++ os-app/open-write/main.svelte
@@ -990,10 +990,6 @@ const mod = {
 	DataStorageClient (inputData) {
 		return zerodatawrap.ZDRWrap({
 			ZDRParamLibrary: (function() {
-				if (inputData === zerodatawrap.ZDRProtocolFission()) {
-					return webnative;
-				}
-
 				return RemoteStorage;
 			})(),
 			ZDRParamScopes: [{
